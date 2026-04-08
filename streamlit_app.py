33bef5f6955a2bc91675aeaaf4fde51eb7064f65
Huzie111 <110819348+Huzie111@users.noreply.github.com>
2026-04-08 11:44:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ CLASS_NAMES = ['COVID-19', 'Other Disease', 'Healthy']
 device = 'cpu'
 
 # Google Drive file ID - REPLACE WITH YOUR ACTUAL ID
-FILE_ID = "1df5sydv-k8nmIh83mWK-vE8lxTfYQG9C"  # <--- CHANGE THIS
+FILE_ID = ""  # <--- CHANGE THIS
 MODEL_PATH = "pytorch_model.bin"
 
 @st.cache_resource
